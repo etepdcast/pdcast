fd4b2c29e6cc2ce9fc95fcb97608dee34a90f643
Taciano Amorim <tacianolamorim@gmail.com>
2019-11-26 12:00:19 -0300

    Adicinado a lista de podcast

--- pdcast/lib/src/view/home_widget.dart
+++ pdcast/lib/src/view/home_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:pdcast/src/model/choice.dart';
 
 class HomeWidget extends StatefulWidget {
   @override
@@ -7,7 +8,29 @@ class HomeWidget extends StatefulWidget {
 }
 
 class _HomeWidgetState extends State<HomeWidget> with SingleTickerProviderStateMixin{
-@override
+  
+  int _selectedIndex= 0;
+  
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex= index;
+    });
+  }
+
+  Choice _selectedChoice = choices[0]; // The app's "state".
+
+  void _select(Choice choice) {
+    // Causes the app to rebuild with the new _selectedChoice.
+    setState(() {
+      _selectedChoice = choice;
+    });
+  }  
+  
+  final List<String> entries = <String>['Post A', 'Post B',
+    'Post C', 'Post D', 'Post E', 'Post F', 'Post G'];
+  final List<int> colors = <int>[600, 500, 100, 800, 600, 500, 100];
+
+  @override
   Widget build(BuildContext context) {
 
     // Fixa a tela do app
@@ -22,6 +45,20 @@ class _HomeWidgetState extends State<HomeWidget> with SingleTickerProviderStateM
           appBar: AppBar(
             backgroundColor: Colors.black,
             title: Text('PdCast'),
+            actions: <Widget>[
+              PopupMenuButton<Choice>(
+                onSelected: _select,
+                itemBuilder: (BuildContext context) {
+                  return choices.skip(0).map((Choice choice) {
+                    return PopupMenuItem<Choice>(
+                      value: choice,
+                      child: Text(choice.title),
+                    );
+                  }).toList();
+                },
+              ),
+            ],
+
             bottom: TabBar(
               tabs: [
                 Tab(
@@ -42,7 +79,18 @@ class _HomeWidgetState extends State<HomeWidget> with SingleTickerProviderStateM
           ),
           body: TabBarView(
             children: [
-              Icon(Icons.new_releases),
+              ListView.separated(
+                padding: const EdgeInsets.all(8),
+                itemCount: entries.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 50,
+                    color: Colors.amber[colors[index]],
+                    child: Center(child: Text('Entry ${entries[index]}')),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) => const Divider(),
+              ),
               Icon(Icons.favorite),
               Icon(Icons.file_download),
             ],
@@ -64,15 +112,44 @@ class _HomeWidgetState extends State<HomeWidget> with SingleTickerProviderStateM
                 title: Text('School'),
               ),
             ],
-            //currentIndex: _selectedIndex,
+            currentIndex: _selectedIndex,
             selectedItemColor: Colors.amber[800],
-           // onTap: _onItemTapped,
-          ),           
+            onTap: _onItemTapped,
+          ),
         ),
       ),
     );
   }
 } 
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'Meus canais', icon: Icons.directions_car),
+  const Choice(title: 'Configuração', icon: Icons.directions_bike),
+];
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({Key key, this.choice}) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = Theme.of(context).textTheme.display1;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(choice.icon, size: 128.0, color: textStyle.color),
+            Text(choice.title, style: textStyle),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 //   @override
 //   Widget build(BuildContext context) {
